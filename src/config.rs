346f75857cddd449010e5e9361c1583d520ba818
wfrsk <wfrsk@proton.me>
2023-01-25 15:08:26 +0100

    use tabs

--- src/config.rs
+++ src/config.rs
@@ -5,86 +5,87 @@ use std::path::PathBuf;
 
 #[derive(Deserialize)]
 pub struct Config {
-    pub socket: PathBuf,
-    pub session: Vec<Session>,
+	pub socket: PathBuf,
+	pub session: Vec<Session>,
 }
 
 #[derive(Deserialize, Clone)]
 pub struct Session {
-    pub name: String,
-    pub database: PathBuf,
-    pub keyfile: Option<PathBuf>,
-    pub alive: u32,
-    pub clipboard: u32,
-    pub command: Command,
+	pub name: String,
+	pub database: PathBuf,
+	pub keyfile: Option<PathBuf>,
+	pub alive: u32,
+	pub clipboard: u32,
+	pub command: Command,
 }
 
 #[derive(Deserialize, Clone)]
 pub struct Command {
-    pub pass: Vec<String>,
-    pub list: Vec<String>,
+	pub pass: Vec<String>,
+	pub list: Vec<String>,
 }
 
 impl Config {
-    #[inline(always)]
-    pub fn process(mut self) -> Self {
-        self.socket = expand_path(&self.socket);
-        self.session.iter_mut().for_each(process_session);
+	#[inline(always)]
+	pub fn process(mut self) -> Self {
+		self.socket = expand_path(&self.socket);
+		self.session.iter_mut().for_each(process_session);
 
-        #[inline(always)]
-        #[allow(deprecated)] // suppress `env::home_dir` warning (doesn't matter, since we target linux anyways)
-        fn expand_path(path: &PathBuf) -> PathBuf {
-            PathBuf::from(
-                &(*full_with_context_no_errors(
-                    path.to_str().expect("path contains invalid unicode"),
-                    || {
-                        env::home_dir().map(|path| {
-                            path.to_str()
-                                .expect("path contains invalid unicode")
-                                .to_string()
-                        })
-                    },
-                    |identifier| env::var(identifier).ok(),
-                )),
-            )
-        }
+		#[inline(always)]
+		#[allow(deprecated)] // suppress `env::home_dir` warning (doesn't matter, since we target linux anyways)
+		fn expand_path(path: &PathBuf) -> PathBuf {
+			PathBuf::from(
+				&(*full_with_context_no_errors(
+					path.to_str().expect("path contains invalid unicode"),
+					|| {
+						env::home_dir().map(|path| {
+							path.to_str()
+								.expect("path contains invalid unicode")
+								.to_string()
+						})
+					},
+					|identifier| env::var(identifier).ok(),
+				)),
+			)
+		}
 
-        #[inline(always)]
-        fn process_session(
-            Session {
-                name,
-                ref mut database,
-                ref mut keyfile,
-                ref mut command,
-                ..
-            }: &mut Session,
-        ) {
-            *database = expand_path(&database);
-            keyfile.as_mut().map(|keyfile| expand_path(keyfile));
-            process_command(command, &name);
-        }
+		#[inline(always)]
+		fn process_session(
+			Session {
+				name,
+				ref mut database,
+				ref mut keyfile,
+				ref mut command,
+				..
+			}: &mut Session,
+		) {
+			*database = expand_path(&database);
+			keyfile.as_mut().map(|keyfile| expand_path(keyfile));
+			process_command(command, &name);
+		}
 
-        #[inline(always)]
-        #[allow(deprecated)]
-        fn process_command(cmd: &mut Command, session_name: &str) {
-            cmd.pass
-                .iter_mut()
-                .chain(cmd.list.iter_mut())
-                .for_each(|arg| {
-                    *arg = String::from(full_with_context_no_errors(
-                        &arg.replace("{session.name}", session_name),
-                        || {
-                            env::home_dir().map(|path| {
-                                path.to_str()
-                                    .expect("path contains invalid unicode")
-                                    .to_string()
-                            })
-                        },
-                        |identifier| env::var(identifier).ok(),
-                    ))
-                });
-        }
+		#[inline(always)]
+		#[allow(deprecated)]
+		fn process_command(cmd: &mut Command, session_name: &str) {
+			cmd.pass
+				.iter_mut()
+				.chain(cmd.list.iter_mut())
+				.for_each(|arg| {
+					*arg = String::from(full_with_context_no_errors(
+						&arg.replace("{session.name}", session_name),
+						|| {
+							env::home_dir().map(|path| {
+								path.to_str()
+									.expect("path contains invalid unicode")
+									.to_string()
+							})
+						},
+						|identifier| env::var(identifier).ok(),
+					))
+				});
+		}
 
-        self
-    }
+		self
+	}
 }
+
--- src/server.rs
+++ src/server.rs
@@ -21,108 +21,109 @@ use clap::crate_version;
 pub const VERSION: &'static str = crate_version!();
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Default config path
-    let xdgd = xdg::BaseDirectories::with_prefix("rkeep").unwrap();
-    let xdgc = xdgd.place_config_file("config.toml").unwrap();
-    let default = xdgc.to_str().unwrap();
+	// Default config path
+	let xdgd = xdg::BaseDirectories::with_prefix("rkeep").unwrap();
+	let xdgc = xdgd.place_config_file("config.toml").unwrap();
+	let default = xdgc.to_str().unwrap();
 
-    // Shell args
-    let args = clap_app!(rkeepd =>
-        (version: VERSION)
-        (author: crate_authors!())
-        (about: "Persistent KeePass backend with display hooks")
-        (@arg c: -c --config <FILE> +takes_value default_value(default) "Configuration file")
-    )
-    .get_matches();
+	// Shell args
+	let args = clap_app!(rkeepd =>
+		(version: VERSION)
+		(author: crate_authors!())
+		(about: "Persistent KeePass backend with display hooks")
+		(@arg c: -c --config <FILE> +takes_value default_value(default) "Configuration file")
+	)
+	.get_matches();
 
-    // Load config
-    let config_file = args.value_of("c").unwrap_or(&default);
-    let config_str = fs::read_to_string(&config_file)?;
-    let mut config: Config = toml::from_str::<Config>(&config_str).unwrap().process();
+	// Load config
+	let config_file = args.value_of("c").unwrap_or(&default);
+	let config_str = fs::read_to_string(&config_file)?;
+	let mut config: Config = toml::from_str::<Config>(&config_str).unwrap().process();
 
-    // Set up sessions
-    let sessions = Arc::new(Mutex::new(HashMap::<String, keepass::Session>::new()));
-    for session in &mut config.session {
-        sessions
-            .lock()
-            .unwrap()
-            .insert(session.name.clone(), keepass::Session::new(&session));
-    }
+	// Set up sessions
+	let sessions = Arc::new(Mutex::new(HashMap::<String, keepass::Session>::new()));
+	for session in &mut config.session {
+		sessions
+			.lock()
+			.unwrap()
+			.insert(session.name.clone(), keepass::Session::new(&session));
+	}
 
-    // Set up cleaning thread
-    cleaning(Arc::clone(&sessions));
+	// Set up cleaning thread
+	cleaning(Arc::clone(&sessions));
 
-    // Start listening for clients
-    let _ = fs::remove_file(&config.socket);
-    let listener = UnixListener::bind(config.socket)?;
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                if let Err(e) = client(&mut sessions.lock().unwrap(), stream) {
-                    println!("{}", e.to_string());
-                }
-            }
-            Err(_) => {
-                continue;
-            }
-        }
-    }
+	// Start listening for clients
+	let _ = fs::remove_file(&config.socket);
+	let listener = UnixListener::bind(config.socket)?;
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				if let Err(e) = client(&mut sessions.lock().unwrap(), stream) {
+					println!("{}", e.to_string());
+				}
+			}
+			Err(_) => {
+				continue;
+			}
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
 
 fn cleaning(sessions: Arc<Mutex<HashMap<String, keepass::Session>>>) {
-    thread::spawn(move || loop {
-        for (_, session) in sessions.lock().unwrap().iter_mut() {
-            if let Err(e) = session.clean() {
-                println!("{}", e.to_string());
-            }
-        }
+	thread::spawn(move || loop {
+		for (_, session) in sessions.lock().unwrap().iter_mut() {
+			if let Err(e) = session.clean() {
+				println!("{}", e.to_string());
+			}
+		}
 
-        thread::sleep(Duration::new(1, 0));
-    });
+		thread::sleep(Duration::new(1, 0));
+	});
 }
 
 fn client(
-    sessions: &mut HashMap<String, keepass::Session>,
-    stream: UnixStream,
+	sessions: &mut HashMap<String, keepass::Session>,
+	stream: UnixStream,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let stream = BufReader::new(stream);
-    for line in stream.lines() {
-        let command = line.unwrap();
-        let v: Vec<&str> = command.split("|").collect();
-        let (s, c) = (v[0], v[1]);
+	let stream = BufReader::new(stream);
+	for line in stream.lines() {
+		let command = line.unwrap();
+		let v: Vec<&str> = command.split("|").collect();
+		let (s, c) = (v[0], v[1]);
 
-        // Check if session exists by name
-        if sessions.contains_key(s) {
-            let session = &mut sessions.get_mut(s).unwrap();
+		// Check if session exists by name
+		if sessions.contains_key(s) {
+			let session = &mut sessions.get_mut(s).unwrap();
 
-            // Open if not yet open
-            if !session.database.is_some() {
-                session.open(&display::password(&session.command.pass)?)?;
-            }
+			// Open if not yet open
+			if !session.database.is_some() {
+				session.open(&display::password(&session.command.pass)?)?;
+			}
 
-            match c {
-                // Execute keepass backend and display frontend
-                "exec" => {
-                    let list = session.list()?;
-                    let entry = display::list(&session.command.list, &list)?;
-                    session.clip(&entry)?;
+			match c {
+				// Execute keepass backend and display frontend
+				"exec" => {
+					let list = session.list()?;
+					let entry = display::list(&session.command.list, &list)?;
+					session.clip(&entry)?;
 
-                    // Since the user just clipped a new password
-                    // reset clipboard timeouts on other sessions
-                    for (key, other) in sessions {
-                        if key != s {
-                            other.clip_reset();
-                        }
-                    }
+					// Since the user just clipped a new password
+					// reset clipboard timeouts on other sessions
+					for (key, other) in sessions {
+						if key != s {
+							other.clip_reset();
+						}
+					}
 
-                    break;
-                }
-                _ => break,
-            }
-        }
-    }
+					break;
+				}
+				_ => break,
+			}
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
+
